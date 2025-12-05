68f577bb4482bdcce2abf90841278c0606d66620
Aidan Chang <119556552+plinygg@users.noreply.github.com>
2025-12-04 23:55:28 -0600

    added beginning of navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar/Navbar.jsx'
 import './App.css'
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div>
+      <div>
+        <Navbar />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
