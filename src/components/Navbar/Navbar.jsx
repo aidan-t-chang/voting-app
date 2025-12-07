b8e4b6d48e376861d0356f60e4f2e3d78f60f2a2
Aidan Chang <119556552+plinygg@users.noreply.github.com>
2025-12-07 00:44:58 -0600

    added elementary navbar and elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,13 +4,12 @@ import './Navbar.css';
 function Navbar() {
     return (
         <nav className="navbar">
-            <div className="navbar-logo">
-                <a href="/">ISMA Food Rating</a>
-            </div>
             <ul className="navbar-links">
+                <li><a href="/" className="navbar-logo-li"><img src="/logo.png" alt="Logo" className="navbar-logo"/></a></li>
                 <li><a href="/">Home</a></li> {/*where users can view highlights (top-rated, lowest-rated)*/} 
                 <li><a href="/vote">Vote</a></li>
                 <li><a href="/listing">Food Listing</a></li>
+                <li><a href="/leaderboard">Leaderboard</a></li>
                 <li><a href="/about">About</a></li>
             </ul>
         </nav>
